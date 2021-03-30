c98c40562f6c6c558a4b663eee8d273cd49507ca
Jakob Hansen <jakob.hansen@hotmail.no>
2021-03-30 17:36:33 +0200

    Fix package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FrontPage from 'frontpage/FrontPage'
 import "App.css"
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import First from 'paaskejakt/First';
 import Second from 'paaskejakt/Second';
 
