493751c070636f0bbb049c7f0a814636ead54120
Jakob Hansen <jakob.hansen@hotmail.no>
2021-05-02 00:47:23 +0200

    "Added socials buttons"

--- src/frontpage/Socials.tsx
+++ src/frontpage/Socials.tsx
@@ -1,9 +1,21 @@
 import headshot from 'resources/headshot.png';
 import 'frontpage/Socials.css'
+import { SocialIcon } from 'react-social-icons';
 
 function Socials() {
     return (
-        <img src={headshot} className="headshot" alt="Headshot" />
+        <div className="socialsContainer">
+            <img src={headshot} className="headshot" alt="Headshot" />
+            <div className="socials">
+                <div className="socialsRow">
+                    <SocialIcon style={{ "height": "8em", "width": "8em" }} className="socialIcon" url="https://www.github.com/jakobkhansen"/>
+                    <SocialIcon style={{ "height":"8em", "width": "8em" }} className="socialIcon" url="https://www.linkedin.com/in/jakob-hansen-b1a9a5174/"   />
+                </div>
+                <div className="socialsRow">
+                    <SocialIcon style={{ "height": "8em", "width": "8em" }} className="socialIcon" url="mailto:jakob.hansen@hotmail.no"  />
+                </div>
+            </div>
+        </div>
     )
 }
 
