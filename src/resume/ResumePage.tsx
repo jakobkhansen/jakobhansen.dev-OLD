ff65859e99eba032b3b7f2de31cccb19bf600d65
Jakob Hansen <jakob.hansen@hotmail.no>
2021-08-06 03:35:45 +0200

    Display resume on resume page and download resume button

--- src/resume/ResumePage.tsx
+++ src/resume/ResumePage.tsx
@@ -1,4 +1,17 @@
+import ResumePDF from "resume/ResumePDF"
+import "resume/ResumePage.css"
+import { Button } from "semantic-ui-react"
+import { Link } from "react-router-dom"
+import resume from "resources/resume2021.pdf"
+
 export default function ResumePage() {
 
-    return <div>Resume coming soon!</div>
+    return (
+        <div style={{ width: "100%" }}>
+
+            <Button className="DownloadButton" as={Link} to={resume} download target="_blank">Download PDF</Button>
+            <ResumePDF />
+
+        </div>
+    )
 }
